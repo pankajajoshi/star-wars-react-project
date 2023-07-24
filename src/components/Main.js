7b963b1e570a1ff5d494e91636abfa30f4f7f8e7
pankajajoshi <joshi.pankaja@gmail.com>
2023-07-24 16:10:04 -0400

    Main text changes
    styles added

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ function Main() {
 	return (
 		<main className="txt-shadow-red">
 			<h1 className="main-title">STAR WARS API</h1>
-			<h2 className="main-title">
+			<h2 className="sub-title">
 				Use the previous and next buttons on each category to view the
 				information provided by the Star Wars API
 			</h2>
